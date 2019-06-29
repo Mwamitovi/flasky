7e0761a85660c21173250ac89738b1672b4a1fcb
Martin Matovu <matovu.synergy@gmail.com>
2019-06-29 16:00:57 +0300

    Update code lines

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
-# first flasky app
-from flask import Flask, url_for
+# hello.py: A complete Flask app
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello, Wordl!</h1>'
+    return '<h1>Hello, Martin!</h1>'
