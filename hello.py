c1413b324f1886b309069117981fce0709206123
Martin Matovu <matovu.synergy@gmail.com>
2019-06-29 18:22:23 +0300

    Include ``request`` to handle more variables

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 # hello.py: A complete Flask app
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
+# static route
 @app.route('/')
 def index():
-    return '<h1>Hello, Martin!</h1>'
+    return '<h1>Hello,World!</h1>'
+
+
+# dynamic route
+@app.route('/user/<name>')
+def user(name):
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Hello, {}!<br/></h1><p>Your browser is {}</p>'.format(name, user_agent)
