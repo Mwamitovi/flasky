c6d0d710ab1b99f492f8d2e9b8bb215a62e2ff86
Martin Matovu <matovu.synergy@gmail.com>
2019-08-10 09:57:48 +0300

    Add a session while handling form data within index()
    Signed-off-by: Martin Matovu <matovu.synergy@gmail.com>

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 # hello.py: A complete Flask app
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, \
+    session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 
@@ -19,12 +20,11 @@ bootstrap = Bootstrap(app)
 # static route
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 # dynamic route
