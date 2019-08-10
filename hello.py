a3cd54840b103de49a62ec42105ab93671b6d5f9
Martin Matovu <matovu.synergy@gmail.com>
2019-08-10 08:37:33 +0300

    use flask_wtf, and define `NameForm` inherits FlaskForm class
    Signed-off-by: Martin Matovu <matovu.synergy@gmail.com>

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 # hello.py: A complete Flask app
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # initialize the app
 app = Flask(__name__)
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+app.config['SECRET_KEY'] = \
+    '7\xb2\xad\xf5\x14l\xd8tOP\xf6\n\xe9\xe1\x92q\xbf\xc6\x92_g\xec \xa5'
 # initialize bootstrap framework
 bootstrap = Bootstrap(app)
 
@@ -30,3 +37,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+# form
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
+
+
+
+
+
+
