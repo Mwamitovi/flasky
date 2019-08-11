3ea2f1ec4dedd6f8995778a8ecf73886817bda40
Martin Matovu <matovu.synergy@gmail.com>
2019-08-11 14:51:30 +0300

    Refactor index(), add db query (if user exists) and db.session handling to save new user.
    Signed-off-by: Martin Matovu <matovu.synergy@gmail.com>

--- hello.py
+++ hello.py
@@ -40,12 +40,23 @@ db = SQLAlchemy(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        _user = User.query.filter_by(username=form.name.data).first()
+        if _user is None:
+            _user = User(username=form.name.data)
+            db.session.add(_user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template(
+        'index.html',
+        form=form,
+        name=session.get('name'),
+        known=session.get('known', False)
+    )
 
 
 # dynamic route
