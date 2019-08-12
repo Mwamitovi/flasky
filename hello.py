910cc1964c526126f40c6eef4119a30390613905
Martin Matovu <matovu.synergy@gmail.com>
2019-08-12 11:33:07 +0300

    Define send_mail() function
    Signed-off-by: Martin Matovu <matovu.synergy@gmail.com>

--- hello.py
+++ hello.py
@@ -129,13 +129,14 @@ def make_shell_context():
 
 
 # email support
-# def send_mail(to, subject, template, **kwargs):
-#     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject), \
-#           sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
-#     _body = render_template(template+'.txt', **kwargs)
-#     msg.body =
-#     msg.html = render_template(template + '.html', **kwargs)
-#     mail.send(msg)
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject),
+    sender=app.config['FLASKY_MAIL_SENDER'],
+    recipients=[to]
+    )
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 
