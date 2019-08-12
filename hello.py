d5b586aba4813dfcc9e8408f4480928e2216b8ca
Martin Matovu <matovu.synergy@gmail.com>
2019-08-12 06:25:52 +0300

    Add SMTP settings to app.config
    Signed-off-by: Martin Matovu <matovu.synergy@gmail.com>

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -32,11 +33,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'da
 # setting false to use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# setting SMTP server
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
+
 # initialize the database
 db = SQLAlchemy(app)
 
 # initialize migrations
 migrate = Migrate(app, db)
+
+# initialize mail
+mail = Mail(app)
 
 
 # static route
