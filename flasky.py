18222b543f6482fa1715947e7592efd2c9ed2d01
Martin Matovu <matovu.synergy@gmail.com>
2019-11-21 13:10:15 +0300

    App_break_up: Define the application instance

---
+++ flasky.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from app import create_app, db
+from app.models import User, Role
+from flask_migrate import Migrate
+
+load_dotenv()
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
